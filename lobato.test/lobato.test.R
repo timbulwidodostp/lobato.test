fe9a25f789c044607e4d52d7e7d2bfd2e5342161
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-10-20 06:54:21 +0700

    lobato.test (nortsTest) With (In) R Software

---
+++ lobato.test/lobato.test.R
@@ -0,0 +1,16 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# The Lobato and Velasco's Test for normality Use lobato.test (nortsTest) With (In) R Software
+install.packages("nortsTest")
+library("nortsTest")
+lobato.test = read.csv("https://raw.githubusercontent.com/timbulwidodostp/lobato.test/main/lobato.test/lobato.test.csv",sep = ";")
+# Estimation The Lobato and Velasco's Test for normality Use lobato.test (nortsTest) With (In) R Software
+lobato.test = lobato.test$lobato.test
+lobato.test <- lobato.test(lobato.test)
+lobato.test
+# The Lobato and Velasco's Test for normality Use lobato.test (nortsTest) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
